4c1ddd98a8dc3ffe328575b3453a12233aabdc0e
xjplus <xxjplus@github.com>
2019-03-04 13:45:55 +0800

    init

---
+++ src/test/java/cn/xuqplus/jvmtest/JvmTestApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.xuqplus.jvmtest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JvmTestApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
