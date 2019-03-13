c238c382086a18ebcdfc7edb91e0baab7ebbb0ed
xjplus <xxjplus@github.com>
2019-03-13 14:26:18 +0800

    多维数组访问

--- src/test/java/cn/xuqplus/jvmtest/day03/ArrayTest.java
+++ src/test/java/cn/xuqplus/jvmtest/day03/ArrayTest.java
@@ -92,4 +92,31 @@ public class ArrayTest {
     Assert.assertFalse(ints.getClass().equals(integers.getClass()));
     Assert.assertFalse(chars.getClass().equals(integers.getClass()));
   }
+
+  @Test
+  public void a2() {
+    int[][] a = {{1, 2}, {3, 4}, {5, 6}};
+    System.out.println(a[0]);
+    System.out.println(a[0][0]);
+    System.out.println(a[0][1]);
+    System.out.println(a[1][0]);
+    System.out.println(a[1][1]);
+    System.out.println(a[2][0]);
+    System.out.println(a[2][1]);
+
+    int[][][] b = {{{0, 1, 2}, {3, 4, 5}, {6, 7, 8}},
+            {{9, 10, 11}, {12, 13, 14}, {15, 16, 17}},
+            {{18, 19, 20}, {21, 22, 23}, {24, 25, 26}}};
+    System.out.println(b[0]);
+    System.out.println(b[0][0]);
+    System.out.println(b[0][0][0]);
+    System.out.println(b[0][0][1]);
+    System.out.println(b[0][0][2]);
+    System.out.println(b[0][1][0]);
+    System.out.println(b[0][1][1]);
+    System.out.println(b[0][1][2]);
+    System.out.println(b[0][2][0]);
+    System.out.println(b[0][2][1]);
+    System.out.println(b[0][2][2]);
+  }
 }
